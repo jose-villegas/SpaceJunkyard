538c12c6968ddc9e8736028c3f24d957b46211f0
Jose Villegas <villegasjose.gg@gmail.com>
2025-01-14 13:01:05 +0100

    Parallelize gravity center update

--- Assets/Source/World/Dynamics/Orbiting/Systems/OrbiterPointUpdateSystem.cs
+++ Assets/Source/World/Dynamics/Orbiting/Systems/OrbiterPointUpdateSystem.cs
@@ -1,4 +1,3 @@
-
 using SpaceJunkyard.World.Astronomical;
 using Unity.Burst;
 using Unity.Collections;
@@ -12,48 +11,60 @@ namespace SpaceJunkyard.World.Dynamics.Orbiting
     {
         private EntityQuery _orbitablesQuery;
         private NativeArray<AstronomicalBody> _orbitableBodies;
-
+        
         public void OnCreate(ref SystemState state)
         {
             // state update
             state.RequireForUpdate<Orbiter>();
-
-            var orbiters = SystemAPI.QueryBuilder().WithAll<Orbiter, OrbiterPoint>().Build();
             var bodies = SystemAPI.QueryBuilder().WithAll<AstronomicalBody, Orbitable>().Build();
-
-            state.RequireAnyForUpdate(orbiters, bodies);
-
+            var orbiters = SystemAPI.QueryBuilder().WithAll<Orbiter, OrbiterPoint>().Build();
+            state.RequireAnyForUpdate(bodies, orbiters);
+            
             _orbitablesQuery = SystemAPI.QueryBuilder().WithAll<AstronomicalBody, Orbitable, LocalTransform>().Build();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            var index = 0;
-
-            if (_orbitableBodies.Length == 0)
+            if (_orbitableBodies.Length == 0 || _orbitableBodies.Length != _orbitablesQuery.CalculateEntityCount())
             {
-                _orbitableBodies = CollectionHelper.CreateNativeArray<AstronomicalBody>(_orbitablesQuery.CalculateEntityCount(), Allocator.Persistent);
-            }
-
-            foreach ((var astronomicalBody, var localTransform) in SystemAPI.Query<RefRW<AstronomicalBody>, RefRO<LocalTransform>>().WithAll<Orbitable>())
-            {
-                // update center position
-                astronomicalBody.ValueRW.GravityCenter = localTransform.ValueRO.Position;
-                // save copy for value updates
-                _orbitableBodies[index++] = astronomicalBody.ValueRO;
+                _orbitableBodies =
+                    CollectionHelper.CreateNativeArray<AstronomicalBody>(_orbitablesQuery.CalculateEntityCount(),
+                        Allocator.Persistent);
             }
 
             // schedule orbiter point update
-            var orbiterPontUpdateJob = new OrbiterPointUpdateJob()
+            var gravityCenterUpdate = new GravityCenterUpdateJob()
             {
                 orbitableBodies = _orbitableBodies,
             };
-            orbiterPontUpdateJob.ScheduleParallel();
+            gravityCenterUpdate.ScheduleParallel();
+            
+            // schedule orbiter point update
+            var orbiterReferenceUpdate = new OrbiterPointReferenceUpdateJob()
+            {
+                orbitableBodies = _orbitableBodies
+            };
+            orbiterReferenceUpdate.ScheduleParallel();
+        }
+        
+        [BurstCompile]
+        public partial struct GravityCenterUpdateJob : IJobEntity
+        {
+            public NativeArray<AstronomicalBody> orbitableBodies;
+
+            public void Execute([EntityIndexInQuery] int index, ref AstronomicalBody astronomicalBody,
+                in LocalTransform localTransform, in Orbitable _)
+            {
+                // update center position
+                astronomicalBody.GravityCenter = localTransform.Position;
+                // save copy for value updates
+                orbitableBodies[index] = astronomicalBody;
+            }
         }
 
         [BurstCompile]
-        public partial struct OrbiterPointUpdateJob : IJobEntity
+        public partial struct OrbiterPointReferenceUpdateJob : IJobEntity
         {
             public NativeArray<AstronomicalBody> orbitableBodies;
 
