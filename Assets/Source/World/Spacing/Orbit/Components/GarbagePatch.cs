aa0b5b0e8a52abaf5ead862681cd3d13757941e8
Jose Villegas <villegasjose.gg@gmail.com>
2025-01-22 11:27:24 +0100

    Small refactor

--- Assets/Source/World/Spacing/Orbit/Components/GarbagePatch.cs
+++ Assets/Source/World/Spacing/Orbit/Components/GarbagePatch.cs
@@ -5,49 +5,26 @@ namespace SpaceJunkyard.World.Spacing
 {
     public struct GarbagePatch : IOrbitalPatch
     {
-        private readonly Entity _orbitableBody;
-        private readonly GarbagePatchesSpawnerConfiguration _spawnConfiguration;
-        private readonly PatchedOrbitableAreaConfiguration _patchedOrbitConfiguration;
-        private readonly float _patchSize;
-        private bool _isActive;
-        private bool _isOccupied;
-        private int _patchIndex;
-
         public OrbitableAreaType OrbitableAreaType => OrbitableAreaType.Gargabe;
-
-        public Entity OrbitableBody => _orbitableBody;
-
-        public GarbagePatchesSpawnerConfiguration SpawnConfiguration => _spawnConfiguration;
-
-        public float PatchSize => _patchSize;
-
-        public bool IsActive
-        {
-            get => _isActive;
-            set => _isActive = value;
-        }
-
-        public PatchedOrbitableAreaConfiguration PatchedOrbitConfiguration => _patchedOrbitConfiguration;
-
-        public int PatchIndex => _patchIndex;
-
-        public bool IsOccupied
-        {
-            get => _isOccupied;
-            set => _isOccupied = value;
-        }
+        public Entity OrbitableBody { get; }
+        public GarbagePatchesSpawnerConfiguration SpawnConfiguration { get; }
+        public float PatchSize { get; }
+        public bool IsActive { get; set; }
+        public PatchedOrbitableAreaConfiguration PatchedOrbitConfiguration { get; }
+        public int PatchIndex { get; }
+        public bool IsOccupied { get; set; }
 
         public GarbagePatch(Entity orbitableBody, GarbagePatchesSpawnerConfiguration spawn,
             PatchedOrbitableAreaConfiguration patchedConfiguration, float patchSize,
             int patchIndex)
         {
-            _orbitableBody = orbitableBody;
-            _spawnConfiguration = spawn;
-            _patchSize = patchSize;
-            _isActive = false;
-            _isOccupied = false;
-            _patchIndex = patchIndex;
-            _patchedOrbitConfiguration = patchedConfiguration;
+            OrbitableBody = orbitableBody;
+            SpawnConfiguration = spawn;
+            PatchSize = patchSize;
+            IsActive = false;
+            IsOccupied = false;
+            PatchIndex = patchIndex;
+            PatchedOrbitConfiguration = patchedConfiguration;
         }
     }
 }
